719f557c4f0633854096df4480327df6833826c0
Bogdan Janosevic <96696175+BogdanJ33@users.noreply.github.com>
2025-12-25 19:54:19 +0100

    feature(search): Implemented search with mongoDB (#4)

---
+++ SearchService/Controllers/SearchController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SearchService.DTO;
+using SearchService.Services.Interfaces;
+
+namespace SearchService.Controllers
+{
+    [ApiController]
+    [Route("api/search")]
+    public sealed class SearchController(ISearchService searchService) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> Search([FromBody] SearchRequest request, CancellationToken ct)
+        {
+            var results = await searchService.SearchAsync(
+                city: request.City,
+                country: request.Country,
+                guests: request.Guests,
+                start: request.Start!.Value,
+                end: request.End!.Value,
+                ct: ct);
+
+            return Ok(results);
+        }
+    }
+}
